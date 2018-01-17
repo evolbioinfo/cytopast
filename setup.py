7bcb7ecba6d88e05f9862b246c5757dc035e3eb9
Anna Zhukova <anna.zhukova@pasteur.fr>
2018-01-17 12:14:58 +0100

    tag version 0.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    version='0.1',
+    version='0.1.1',
     description='Visualisation of PASTML trees.',
     author='Anna Zhukova',
     author_email='anna.zhukova@pasteur.fr',
