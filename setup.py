e2acf5c3fdfedd2719ef576aa7d93263ae5f39ae
Anna Zhukova <anna.zhukova@pasteur.fr>
2018-02-27 18:35:52 +0100

    Updated package version (0.5)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    version='0.4',
+    version='0.5',
     description='Visualisation of PASTML trees.',
     author='Anna Zhukova',
     author_email='anna.zhukova@pasteur.fr',
