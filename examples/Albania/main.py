e71c97bd48434f9e18e7e12ec39f02e0d12040f5
Anna Zhukova <anna.zhukova@pasteur.fr>
2018-04-03 12:01:21 +0200

    dependency on PASTML>=0.6.4 (with gnu99 standard)

--- examples/Albania/main.py
+++ examples/Albania/main.py
@@ -5,12 +5,14 @@ from pastml import JOINT, MARGINAL_APPROXIMATION, MAX_POSTERIORI, MARGINAL, JC, 
 
 DATA_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
 TREE_NWK = os.path.join(DATA_DIR, 'Albanian.tree.152tax.tre')
-STATES_INPUT = os.path.join(DATA_DIR, 'Annotation.Albanian.5chars.txt')
+STATES_INPUT = os.path.join(DATA_DIR, 'data.txt')
 
 if '__main__' == __name__:
     pastml_pipeline(data=STATES_INPUT, tree=TREE_NWK,
                     html=os.path.join(DATA_DIR, 'trees', 'Albanian_tree_initial.html'),
-                    data_sep=',', verbose=True, copy_columns=['Country'], work_dir=os.path.join(DATA_DIR, 'pastml'))
+                    html_compressed=os.path.join(DATA_DIR, 'maps', 'Albanian_map_initial.html'),
+                    data_sep=',', verbose=True, copy_columns=['Country'], work_dir=os.path.join(DATA_DIR, 'pastml'),
+                    all=True)
     # for model in (F81, JC):
     for model in (F81,):
         for method in (JOINT, MAX_POSTERIORI, MARGINAL_APPROXIMATION, MARGINAL, DOWNPASS, ACCTRAN, DELTRAN):
@@ -18,4 +20,4 @@ if '__main__' == __name__:
                             html_compressed=os.path.join(DATA_DIR, 'maps', 'Albanian_map_{}_{}.html'.format(model, method)),
                             html=os.path.join(DATA_DIR, 'trees', 'Albanian_tree_{}_{}.html'.format(model, method)),
                             data_sep=',', model=model, verbose=True, prediction_method=method,
-                            work_dir=os.path.join(DATA_DIR, 'pastml'))
+                            work_dir=os.path.join(DATA_DIR, 'pastml'), all=True)
