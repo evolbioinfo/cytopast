69ac45f20d8c09d6f1527b1d580760954c4c0f8f
Anna Zhukova <zhutchok@gmail.com>
2018-07-20 16:24:28 +0200

    README for Albanian example

--- examples/Albania/main.py
+++ examples/Albania/main.py
@@ -1,21 +1,29 @@
 import os
+from pastml import JOINT, MARGINAL_APPROXIMATION, MAX_POSTERIORI, JC, F81, DOWNPASS, ACCTRAN, DELTRAN
 
 from cytopast.pastml_analyser import pastml_pipeline
-from pastml import JOINT, MARGINAL_APPROXIMATION, MAX_POSTERIORI, MARGINAL, JC, F81, DOWNPASS, ACCTRAN, DELTRAN
 
 DATA_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
 TREE_NWK = os.path.join(DATA_DIR, 'Albanian.tree.152tax.tre')
 STATES_INPUT = os.path.join(DATA_DIR, 'data.txt')
 
 if '__main__' == __name__:
+    # The initial tree without ACR
     pastml_pipeline(data=STATES_INPUT, tree=TREE_NWK,
                     html=os.path.join(DATA_DIR, 'trees', 'Albanian_tree_initial.html'),
-                    html_compressed=os.path.join(DATA_DIR, 'maps', 'Albanian_map_initial.html'),
                     data_sep=',', verbose=True, copy_columns=['Country'])
-    # for model in (F81, JC):
-    for model in (F81,):
-        for method in (JOINT, MAX_POSTERIORI, MARGINAL_APPROXIMATION, MARGINAL, DOWNPASS, ACCTRAN, DELTRAN):
+    # ACR with ML methods
+    for model in (F81, JC):
+        for method in (JOINT, MAX_POSTERIORI, MARGINAL_APPROXIMATION):
             pastml_pipeline(data=STATES_INPUT, tree=TREE_NWK,
-                            html_compressed=os.path.join(DATA_DIR, 'maps', 'Albanian_map_{}_{}.html'.format(model, method)),
-                            html=os.path.join(DATA_DIR, 'trees', 'Albanian_tree_{}_{}.html'.format(model, method)),
+                            html_compressed=os.path.join(DATA_DIR, 'maps',
+                                                         'Albanian_map_{}_{}.html'.format(method, model)),
+                            html=os.path.join(DATA_DIR, 'trees', 'Albanian_tree_{}_{}.html'.format(method, model)),
                             data_sep=',', model=model, verbose=True, prediction_method=method)
+    # ACR with parsimony
+    for method in (DOWNPASS, ACCTRAN, DELTRAN):
+            pastml_pipeline(data=STATES_INPUT, tree=TREE_NWK,
+                            html_compressed=os.path.join(DATA_DIR, 'maps',
+                                                         'Albanian_map_{}.html'.format(method)),
+                            html=os.path.join(DATA_DIR, 'trees', 'Albanian_tree_{}.html'.format(method)),
+                            data_sep=',', verbose=True, prediction_method=method)
