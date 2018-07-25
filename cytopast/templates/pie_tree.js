3b9dcb8ef911b7bbf9b69b10b6f08f5b93abe0c6
Anna Zhukova <zhutchok@gmail.com>
2018-07-25 17:08:50 +0200

    Minor js improvements: full screen etc.

--- cytopast/templates/pie_tree.js
+++ cytopast/templates/pie_tree.js
@@ -1,3 +1,28 @@
+
+var layoutOptions = {
+    name: '{{layout}}',
+    nodesep: 0, // the separation between adjacent nodes in the same rank
+    edgeSep: 1, // the separation between adjacent edges in the same rank
+    rankSep: 40, // the separation between adjacent ranks
+    rankDir: 'TB', // 'TB' for top to bottom flow, 'LR' for left to right,
+    ranker: 'longest-path', // Type of algorithm to assign a rank to each node in the input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
+    minLen: function( edge ){ return edge.data('minLen') | 0; }, // number of ranks to keep between the source and target of the edge
+
+    // general layout options
+    fit: true, // whether to fit to viewport
+    padding: 1, // fit padding
+    spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
+    nodeDimensionsIncludeLabels: true, // whether labels should be included in determining the space used by a node
+    animate: false, // whether to transition the node positions
+    animateFilter: function( node, i ){ return true; }, // whether to animate specific nodes when animation is on; non-animated nodes immediately go to their final positions
+    animationDuration: 500, // duration of animation in ms if enabled
+    animationEasing: undefined, // easing of animation if enabled
+    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    transform: function( node, pos ){ return pos; }, // a function that applies a transform to the final node position
+    ready: function(){}, // on layoutready
+    stop: function(){} // on layoutstop
+  };
+
 var cy = cytoscape({
   container: document.getElementById('cy'),
 
@@ -64,93 +89,68 @@ var cy = cytoscape({
 
   elements: {{elements}},
 
-  layout: {
-    name: '{{layout}}',
-    nodesep: 0, // the separation between adjacent nodes in the same rank
-    edgeSep: 1, // the separation between adjacent edges in the same rank
-    rankSep: 40, // the separation between adjacent ranks
-    rankDir: 'TB', // 'TB' for top to bottom flow, 'LR' for left to right,
-    ranker: 'longest-path', // Type of algorithm to assign a rank to each node in the input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
-    minLen: function( edge ){ return edge.data('minLen') | 0; }, // number of ranks to keep between the source and target of the edge
-
-    // general layout options
-    fit: true, // whether to fit to viewport
-    padding: 1, // fit padding
-    spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
-    nodeDimensionsIncludeLabels: true, // whether labels should be included in determining the space used by a node
-    animate: false, // whether to transition the node positions
-    animateFilter: function( node, i ){ return true; }, // whether to animate specific nodes when animation is on; non-animated nodes immediately go to their final positions
-    animationDuration: 500, // duration of animation in ms if enabled
-    animationEasing: undefined, // easing of animation if enabled
-    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-    transform: function( node, pos ){ return pos; }, // a function that applies a transform to the final node position
-    ready: function(){}, // on layoutready
-    stop: function(){} // on layoutstop
-  },
+  layout: layoutOptions,
 
   ready: function(){
     window.cy = this;
   }
 });
 
-function removeSubtree(node) {
-    var outEdges = cy.edges('edge[source="' + node.id() + '"]');
-    outEdges.targets().forEach(function(ele, i, eles) {
-        removeSubtree(ele);
-    });
-    cy.remove(node);
-}
-
-// remove nodes on right-click
-cy.on('cxttap', 'node', function(evt){
-  removeSubtree(evt.target);
-});
-
-cy.filter(function(ele, i, eles) {
-    return ele.isNode() && ele.data('tooltip') !== undefined;
-} ).qtip({
-    content: function(){
-            var tooltip = this.data('tooltip');
-            tooltip += '<br>id' + (this.data('node_meta') !== undefined ? 's: ': ': ') + this.data('node_root_id')
-            + (this.data('node_meta') !== undefined ? ', ...': '');
-            if (this.data('node_in_tips') !== undefined) {
-                tooltip += '<br>tips inside: ' + this.data('node_in_tips');
-            }
-            if (this.data('node_all_tips') !== undefined) {
-                tooltip += '<br>total tips in the subtree: ' + this.data('node_all_tips');
-            }
-            return tooltip;
+function addQtips() {
+    cy.filter(function(ele, i, eles) {
+        return ele.isNode() && ele.data('tooltip') !== undefined;
+    } ).qtip({
+        content: function(){
+                var tooltip = this.data('tooltip');
+                tooltip += '<br>id' + (this.data('node_meta') !== undefined ? 's: ': ': ') + this.data('node_root_id')
+                + (this.data('node_meta') !== undefined ? ', ...': '');
+                if (this.data('node_in_tips') !== undefined) {
+                    tooltip += '<br>tips inside: ' + this.data('node_in_tips');
+                }
+                if (this.data('node_all_tips') !== undefined) {
+                    tooltip += '<br>total tips in the subtree: ' + this.data('node_all_tips');
+                }
+                return tooltip;
+            },
+        show: {event: 'mouseover'},
+        hide: {event: 'mouseout'},
+        style: {
+                classes: 'qtip-bootstrap',
         },
-    show: {event: 'mouseover'},
-    hide: {event: 'mouseout'},
-    style: {
-            classes: 'qtip-bootstrap',
-    },
-    position: {
-        at: 'center center',
-    }
-});
-
-function removeSubtree(node) {
-    var outEdges = cy.edges('edge[source="' + node.id() + '"]');
-    outEdges.targets().forEach(function(ele, i, eles) {
-        removeSubtree(ele);
+        position: {
+            at: 'center center',
+        }
     });
-    cy.remove(node);
 }
 
+cy.minZoom(.1);
+cy.maxZoom(20);
+addQtips();
+
+//function removeSubtree(node) {
+//    var outEdges = cy.edges('edge[source="' + node.id() + '"]');
+//    outEdges.targets().forEach(function(ele, i, eles) {
+//        removeSubtree(ele);
+//    });
+//    cy.remove(node);
+//}
+//
 // remove nodes on right-click
-cy.on('cxttap', 'node', function(evt){
-  removeSubtree(evt.target);
-});
+//cy.on('cxttap', 'node', function(evt){
+//    removeSubtree(evt.target);
+//});
 
 
-function to_image(){
+function toImage(){
     document.getElementById("downloader").href = cy.jpg({ full: false, quality: 1.0, scale: 2}).replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 }
 
 function fit() {
     cy.fit();
+}
+
+function resetLayout() {
+    cy.layout(layoutOptions).run();
 }
 
 var slider = document.getElementById("myRange");
@@ -159,7 +159,7 @@ if (slider !== null) {
     output.innerHTML = slider.value; // Display the default slider value
 
     // Update the current slider value (each time you drag the slider handle)
-    var removed = cy.collection("[date>{{max_date}}]");
+    var removed = cy.collection();
 
     slider.oninput = function() {
         output.innerHTML = this.value;
@@ -194,4 +194,30 @@ if (slider !== null) {
             node.data('node_fontsize', node.data('node_fontsize_' + this.value));
         }
     }
+}
+
+function toggleFullScreen() {
+  var elem = document.getElementById("fullscreenDiv");
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (elem.requestFullScreen) {
+      elem.requestFullScreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullScreen) {
+      elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+    document.getElementById("fsspan").className = "glyphicon glyphicon-resize-small"
+    document.getElementById("fstext").innerHTML = "  Exit full screen"
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+    document.getElementById("fsspan").className = "glyphicon glyphicon-fullscreen"
+    document.getElementById("fstext").innerHTML = "  Full screen"
+  }
 }
